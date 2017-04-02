e92650a47bf8852b20316777efa5fd28ff29213e
Richard <richard.elder.ii@gmail.com>
2017-04-02 17:59:41 -0400

    ImageViews - connected

---
+++ BooksLibrary/BookViewController.swift
@@ -0,0 +1,33 @@
+//
+//  BookViewController.swift
+//  BooksLibrary
+//
+//  Created by Richard on 4/2/17.
+//  Copyright © 2017 ELION. All rights reserved.
+//
+
+import UIKit
+
+class BookViewController: UIViewController {
+    
+    @IBOutlet weak var bookImageView: UIImageView!
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+}
