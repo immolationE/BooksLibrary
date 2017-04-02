0c5788e2de29cac33b057ffa04877ed01fad72c8
Richard <richard.elder.ii@gmail.com>
2017-04-02 18:13:41 -0400

    ImagePickerController

--- BooksLibrary/BookViewController.swift
+++ BooksLibrary/BookViewController.swift
@@ -8,20 +8,27 @@
 
 import UIKit
 
-class BookViewController: UIViewController {
+class BookViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var bookImageView: UIImageView!
-    
     @IBOutlet weak var titleTextField: UITextField!
     
+    var imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imagePicker.delegate = self
 
         // Do any additional setup after loading the view.
     }
     
     @IBAction func photosTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+        
     }
     
     @IBAction func cameraTapped(_ sender: Any) {
